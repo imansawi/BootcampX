4d4abad5989a96d71da2438f7c1d680f547023e5
imansawi <imansawi@hotmail.com>
2021-03-02 23:40:42 +0000

    added 3_queries_group_by using aggregate functions

--- 2_queries_joins/join-cohort-assignments.sql
+++ 2_queries_joins/join-cohort-assignments.sql
@@ -1,6 +1,5 @@
-SELECT sum(assignment_submissions.duration)
-FROM assignment_submissions INNER JOIN students
-ON assignment_submissions.student_id = students.id
-AND cohorts INNER JOIN students
-ON cohorts.id = students.cohort_id
-AND cohort.date = 'FEB12';
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
