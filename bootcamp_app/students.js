2dd6ca935b319fb74e03c1da270dd03a6cbcebf1
imansawi <imansawi@hotmail.com>
2021-03-04 18:21:41 +0000

    Added file teachers.js to query about teachers

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,13 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const commandLineValue = Number(process.argv[2]);
+const args = process.argv[2];
 
-if (commandLineValue) {
-  const queryValues = [commandLineValue]
-  pool.query(`
+pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE '%${args}%'
 LIMIT ${process.argv[3] || 5};
 `)
 .then(res => {
@@ -23,5 +21,3 @@ LIMIT ${process.argv[3] || 5};
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
-
-}
