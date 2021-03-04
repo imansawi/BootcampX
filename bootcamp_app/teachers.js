5aef5987317bef37608b1c61d22b13f7dacd89d5
imansawi <imansawi@hotmail.com>
2021-03-04 19:39:05 +0000

    Modified students.js and teachers.js by sanitized them, avoided SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const args = process.argv[2];
+const cohortName =  process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+console.log(values);
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -15,9 +18,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
